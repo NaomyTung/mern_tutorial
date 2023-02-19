6e3b8c17fd2472be97e079bf0a6bd4e4296e08e8
NaomyTung <106546446+NaomyTung@users.noreply.github.com>
2023-02-19 14:38:36 -0700

    goals controller and routes setup

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,23 @@
 /**
  * npm i express dotenv mongoose colors
  * npm i -D nodemon
+ * 
+ * npm i express-async-handler
  */
-console.log("Hii")
+
+const express = require('express')
+const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+//overwrite default error handler
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
