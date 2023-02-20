af3c76f16d4c5850ee399bee72fb522078d3c094
NaomyTung <106546446+NaomyTung@users.noreply.github.com>
2023-02-20 10:21:32 -0700

    REST API for goals

---
+++ backend/models/goalModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const goalSchema = mongoose.Schema({
+    text: {
+        type: String,
+        required: [true, 'Please add a text value'],
+     },
+    }, 
+    {
+        timestamps: true,
+    }
+)
+
+//Goal is the name of the model, each instance. When it goes to the DB, it will auto be added to a collection
+//goal w no caps letter and s at the end. If it was Goall --> goalls 
+module.exports = mongoose.model('Goal', goalSchema)
